052c121f278061aac7c742ec8d7cf0cc77625495
bgenia <xbgenia@gmail.com>
2025-04-27 23:27:49 +0300

    fix: fix cookie domain

--- src/classes/AppCookie.ts
+++ src/classes/AppCookie.ts
@@ -9,12 +9,7 @@ interface CookieMap {
 
 export class AppCookie {
     static readonly url = (config.url.api.split('//')[1].includes(':') ? '' : '.') +
-        config.url.api
-        .split('//')
-        .slice(-1)[0]
-        .split(':')[0]
-        .split('.')
-        .slice(-2).join('.'); // so that it works both on localhost and in production
+        config.url.api.split('//').at(-1).split('.').slice(1).join('.'); // so that it works both on localhost and in production
 
     public static get<K extends keyof CookieMap>(key: K): CookieMap[K] | undefined {
         return cookie.get(key) 
